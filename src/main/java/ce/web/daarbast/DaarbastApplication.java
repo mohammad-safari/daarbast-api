1c40448c1e3836bb28a05b0326f3f6f65765cca5
Mohammad-Safari <msafari8051@gmail.com>
2024-05-18 23:31:10 +0330

    [REV]0c6af2d0 desc:migrate to java

---
+++ src/main/java/ce/web/daarbast/DaarbastApplication.java
@@ -0,0 +1,13 @@
+package ce.web.daarbast;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaarbastApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaarbastApplication.class, args);
+	}
+
+}
