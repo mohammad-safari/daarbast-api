0c6af2d02c8d1f1a87f2b3514eb993424c70ae20
Mohammad-Safari <msafari8051@gmail.com>
2024-05-18 23:22:43 +0330

    [INIT]bootstrapped the project

---
+++ src/main/kotlin/ce/web/daarbast/DaarbastApplication.kt
@@ -0,0 +1,11 @@
+package ce.web.daarbast
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DaarbastApplication
+
+fun main(args: Array<String>) {
+	runApplication<DaarbastApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/demo/DemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package ce.web.daarbast
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class daarbastApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
