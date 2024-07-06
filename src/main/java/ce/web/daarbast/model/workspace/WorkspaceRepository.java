98578ec675b445373274b1dfd0dfa8645d3692d4
Mohammad-Safari <msafari8051@gmail.com>
2024-07-06 23:06:50 +0330

    [ADD]workspace by roles user refernece

--- src/main/java/ce/web/daarbast/model/workspace/WorkspaceRepository.java
+++ src/main/java/ce/web/daarbast/model/workspace/WorkspaceRepository.java
@@ -1,8 +1,14 @@
 package ce.web.daarbast.model.workspace;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface WorkspaceRepository extends JpaRepository<Workspace, Long> {
+    @Query("SELECT r.workspace FROM UserWorkspaceRole r WHERE r.user.userId = :userId")
+    List<Workspace> findWorkspacesByUserId(@Param("userId") String userId);
 }
