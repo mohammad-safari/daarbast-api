bc145b97a7f329fea25a3ec787fd9f44b9d8e304
Mohammad-Safari <msafari8051@gmail.com>
2024-07-06 16:55:58 +0330

    [ADD]username search api

--- src/main/java/ce/web/daarbast/model/user/UserRepository.java
+++ src/main/java/ce/web/daarbast/model/user/UserRepository.java
@@ -1,11 +1,16 @@
 package ce.web.daarbast.model.user;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, String> {
     Optional<User> findByUsername(String username);
+    @Query("SELECT u FROM User u WHERE u.username LIKE %:username%")
+    List<User> findByUsernameContaining(@Param("username") String username);
 }
