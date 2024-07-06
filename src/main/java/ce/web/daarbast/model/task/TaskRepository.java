a58b08b0faf4537fcc5d6b0da0d5d8e59bcf019d
Mohammad-Safari <msafari8051@gmail.com>
2024-07-06 22:46:00 +0330

    [ADD]tasks by workspace reference

--- src/main/java/ce/web/daarbast/model/task/TaskRepository.java
+++ src/main/java/ce/web/daarbast/model/task/TaskRepository.java
@@ -1,8 +1,13 @@
 package ce.web.daarbast.model.task;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface TaskRepository extends JpaRepository<Task, Long> {
-}
+    @Query("SELECT t FROM Task t WHERE t.workspace.id = :workspaceId")
+    List<Task> findTasksByWorkspaceId(@Param("workspaceId") Long workspaceId);}
